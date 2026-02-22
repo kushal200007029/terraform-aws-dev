0991a2ed542e16adce37636cda4a516f4b25da60
kushal <kkanhake@gmail.com>
2026-02-23 00:03:31 +0530

    Updated output file

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,13 @@
 #   description = "VPC ID"
 #   value       = aws_vpc.main.id
 # }
+
+output "public_instance_ip" {
+  description = "Public IP of public EC2"
+  value       = aws_instance.public_ec2.public_ip
+}
+
+output "private_instance_ip" {
+  description = "Private IP of private EC2"
+  value       = aws_instance.private_ec2.private_ip
+}
